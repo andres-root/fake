ff1e91b75841a45d36de9dd297d06bc628748059
andres-root <andres.root.coder@gmail.com>
2015-01-26 16:09:22 -0500

    setting up amd

--- content/js/config.js
+++ content/js/config.js
@@ -1,9 +1,14 @@
-xrequire.config({
+require.config({
+	baseUrl: "content/js/",
+	deps: ['main'],
 	paths: {
-		"jquery": "content/components/jquery/dist/jquery.min.js"
-		"underscore": "content/components/underscore/underscore-min.js"
-		"bootstrap": "content/components/bootstrap/dist/js/bootstrap.min.js"
-		"isotope": "content/components/isotope/dist/isotope.pkgd.min.js"
+		"jquery": "content/components/jquery/dist/jquery.min.js",
+		"underscore": "content/components/underscore/underscore-min.js",
+		"bootstrap": "content/components/bootstrap/dist/js/bootstrap.min.js",
+		"isotope": "content/components/isotope/dist/isotope.pkgd.min.js",
 		"masonry": "content/components/masonry/dist/masonry.pkgd.min.js"
+	},
+	shim: {
+
 	}
 });
