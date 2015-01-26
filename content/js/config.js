a9379956b25fe8ace1c1a3e2392a9776b7ac0429
andres-root <andres.root.coder@gmail.com>
2015-01-26 15:11:57 -0500

    added masonry libs

---
+++ content/js/config.js
@@ -0,0 +1,9 @@
+xrequire.config({
+	paths: {
+		"jquery": "content/components/jquery/dist/jquery.min.js"
+		"underscore": "content/components/underscore/underscore-min.js"
+		"bootstrap": "content/components/bootstrap/dist/js/bootstrap.min.js"
+		"isotope": "content/components/isotope/dist/isotope.pkgd.min.js"
+		"masonry": "content/components/masonry/dist/masonry.pkgd.min.js"
+	}
+});
