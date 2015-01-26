3a5323397c1f987536373eb3a64116e62423b2da
andres-root <andres.root.coder@gmail.com>
2015-01-26 09:43:04 -0500

    added compiled conf

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,15 @@
 
 var gulp = require('gulp');
 var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+
+var contentPath = 'content/js/';
+var staticPath = 'static/js/';
+
+gulp.task('default', function() {
+	return gulp.src('main.js')
+		.pipe(gulp.dest(contentPath))
+		.pipe(uglify())
+		.pipe(rename({extname: ".min.js"}))
+		.pipe(gulp.dest(staticPath));
+});
