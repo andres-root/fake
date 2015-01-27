c94cedf53509f084474e5694ec5aeea05a507d9b
andres-root <andres.root.coder@gmail.com>
2015-01-27 10:01:22 -0500

    setting up webpack

--- content/js/main.js
+++
@@ -1,6 +0,0 @@
-define(function (require){
-	'use strict';
-	var say = require('say');
-	var result = say.create("Andres");
-	console.log(result);
-});
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+{
+    context: "/app",
+    entry: "./main",
+    output: {
+        path: "/static/js",
+        filename: "bundle.js"
+    }
+}
