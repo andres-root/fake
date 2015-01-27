7272656bb03a250354840e4cc3d76a45b778dd52
andres-root <andres.root.coder@gmail.com>
2015-01-27 09:53:12 -0500

    adding webpack

--- content/js/say.js
+++
@@ -1,9 +0,0 @@
-define(function (require) {
-	'use strict';
-	var Hello = require('hello');
-	var exports = require('exports');
-	function create(name){
-		return new Hello(name);
-	}
-	exports.create = create;
-});
